549f3f5d061ddb9bd70498df778a70d78338494b
Active Collab <Active Collab>
2016-06-07 14:37:59 +0200

    First version

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# New Task Form
+How to create form to create a task in Active Collab
+=======
 
-An intership project here at Active Collab HQ with a goal of creating simple, reusable script that will let you easily create forms on your website that create tasks in your Active Collab.
+
+# How to use this form on your web site
+
+
+If you want to include this form on your web site just take a div with id="main-form" and include it into your web site. After adding this you need to copy configure.php file. That file is page that is going to open after successful send.You can modify HTML as you wish. And your name of your company, name of the app that you are developing, your email address and password in:
+```php
+$authenticator = new \ActiveCollab\SDK\Authenticator\Cloud('name of your company', 'name of the app that you are developing', 'your email address', 'password');
+```
+
+And add you Active Collab instance id:
+```php
+$token = $authenticator->issueToken(<here>);
+```
+
+And add your project id into url:
+```php
+$result = $client->post(projects/<here>/tasks, [
+```
+We have add javascript to prevent double submit of the form.
---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Forma</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body background="bg.jpg">
+	<div class="container" id="main-form">
+		<div align="center">
+			<h1>Report a bug</h1>
+		</div>
+		<form action="configure.php" method="post" onsubmit="myButton.disabled = true; return true;">
+			<div class="form-group">
+				<label for="exampleInputPrica">Bug name:</label>
+				<input class="form-control" name="name"  placeholder="Add bug name"  required>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPrica">Code version</label>
+				<input class="form-control" name="code-version"  placeholder="Add code version"  required>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPrica">Browser:</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="browser[]" value="IE"> IE
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="browser[]" value="Chrome"> Chrome
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="browser[]" value="FireFox"> FireFox
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="browser[]" value="Safari"> Safari
+				</label>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPrica">Url</label>
+				<input class="form-control" name="url"  placeholder="Add url"  required>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPrica">Where can be reproduced:</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="produced[]" value="1" /> client cloud account
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="produced[]" value="2" /> client self-hosted account
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="produced[]" value="3" /> all cloud accounts
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="produced[]" value="4" /> development activecollab
+				</label>
+				<label class="checkbox-inline">
+					<input type="checkbox" name="produced[]" value="5" /> everywhere
+				</label>
+			</div>
+			<label for="exampleInput">Is it reproducible</label>
+			<label class="radio-inline">
+				<input type="radio" name="reproducible[]" id="inlineRadio1" value="1"> Yes
+			</label>
+			<label class="radio-inline">
+				<input type="radio" name="reproducible[]" id="inlineRadio2" value="2"> Occasionally
+			</label>
+			<label class="radio-inline">
+				<input type="radio" name="reproducible[]" id="inlineRadio3" value="3"> One Time
+				<label class="radio-inline">
+					<input type="radio" name="reproducible[]" id="inlineRadio4" value="4"> No
+				</label>
+				</label><br><br>
+			<label>Discription</label>
+			<textarea class="form-control" rows="5" name="desc" placeholder="Add discription"  required></textarea><br>
+			<label>Expected Results</label>
+			<textarea class="form-control" rows="5" name="expected" placeholder="Add expected results"  required></textarea><br>
+			<label>Actual Results</label>
+			<textarea class="form-control" rows="5" name="actual" placeholder="Add actual results"  required></textarea><br>
+			<div class="form-group">
+				<label for="exampleInputName">Reported by</label>
+				<input class="form-control" name="reported-by" placeholder="Add your e-mail"  required />
+			</div>
+			<div align="center">
+				<input type="submit" name="myButton" value="Submit" /><br><br><br>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
